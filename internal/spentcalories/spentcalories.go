16bfc5e822c73202e31f2541f9ed6ff582602940
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-03-27 14:49:00 +0300

    Update spentcalories.go

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -45,7 +45,7 @@ func distance(steps int, height float64) float64 {
 
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
-	if duration<0 {
+	if duration<=0 || weight<=0 || height<=0 {
 		return 0
 	}
 
@@ -56,6 +56,9 @@ func meanSpeed(steps int, height float64, duration time.Duration) float64 {
 
 func TrainingInfo(data string, weight, height float64) (string, error) {
 	// TODO: реализовать функцию
+	if weight<=0 || height<=0 {
+		return 0, fmt.Errorf("Некорректный параметр")
+	}
 	steps, sport, duration, err:=parseTraining(data)
 	if err!=nil {
 		log.Println(err)
